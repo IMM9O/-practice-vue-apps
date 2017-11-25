c9ed3a5adab69ef9e67f1d9618cacb9213a31f4b
IMM9O <islam.muhamed.90+github@gmail.com>
2017-11-25 14:24:14 +0200

    add some colors

--- vue-keep/index.js
+++ vue-keep/index.js
@@ -18,7 +18,7 @@ function delete_cookie(name) {
 var app = new Vue({
 	el: '#app',
 	data: {
-		colors: ['#fff', '#1abc9c', '#2ecc71' , '#3498db' , '#9b59b6'],
+		colors: ['#fff', '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50'],
 		note: {
 			title: '',
 		  text: '',
