372a389592cb54cb13788f5da8023360115eaf77
IMM9O <islam.muhamed.90+github@gmail.com>
2017-12-17 09:17:14 +0200

    format code

--- vue-keep/index.js
+++ vue-keep/index.js
@@ -1,55 +1,76 @@
 function bake_cookie(name, value) {
-  var cookie = [name, '=', JSON.stringify(value), '; domain_.', window.location.host.toString(), '; path=/;'].join('');
+  var cookie = [
+    name,
+    "=",
+    JSON.stringify(value),
+    "; domain_.",
+    window.location.host.toString(),
+    "; path=/;"
+  ].join("");
   document.cookie = cookie;
 }
 
 // reads a cookie according to the given name
 function read_cookie(name) {
-  var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  var result = document.cookie.match(new RegExp(name + "=([^;]+)"));
   result = result != null ? JSON.parse(result[1]) : [];
   return result;
 }
 
 function delete_cookie(name) {
-  document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain.', window.location.host.toString()].join('');
+  document.cookie = [
+    name,
+    "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain.",
+    window.location.host.toString()
+  ].join("");
 }
 
-
 var app = new Vue({
-	el: '#app',
-	data: {
-		colors: ['#fff', '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50'],
-		note: {
-			title: '',
-		  text: '',
-			time: '',
-			color: ''
-		},
-		selectedColor: '',
-		notes: read_cookie('notes')
-	},
-	methods: {
-		onSubmit() {
-			this.note.time = new Date().toLocaleString();
-			this.note.color = this.note.color === '' ? '#FFF' : this.note.color;
-			this.notes.push(this.note);
-			this.note = {};
-			bake_cookie('notes', this.notes);
-		},
-		removeNote(index) {
-			this.notes.splice(index,1);
-			bake_cookie('notes', this.notes);
-		}
-	},
-	computed: {
-			filterColor: function () {
-				return this.notes.filter( (item) => {
-					if(this.selectedColor !== '') {
-							return item.color.toLowerCase() === this.selectedColor.toLowerCase();
-					} else {
-						return true;
-					}
-				})
-			}
-   }
-})
+  el: "#app",
+  data: {
+    colors: [
+      "#fff",
+      "#1abc9c",
+      "#2ecc71",
+      "#3498db",
+      "#9b59b6",
+      "#16a085",
+      "#27ae60",
+      "#2980b9",
+      "#8e44ad",
+      "#2c3e50"
+    ],
+    note: {
+      title: "",
+      text: "",
+      time: "",
+      color: ""
+    },
+    selectedColor: "",
+    notes: read_cookie("notes")
+  },
+  methods: {
+    onSubmit() {
+      this.note.time = new Date().toLocaleString();
+      this.note.color = this.note.color === "" ? "#FFF" : this.note.color;
+      this.notes.push(this.note);
+      this.note = {};
+      bake_cookie("notes", this.notes);
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+      bake_cookie("notes", this.notes);
+    }
+  },
+  computed: {
+    filterColor: function() {
+      return this.notes.filter(item => {
+        if (this.selectedColor !== "") {
+          return item.color.toLowerCase() === this.selectedColor.toLowerCase();
+        } else {
+          return true;
+        }
+      });
+    }
+  }
+});
