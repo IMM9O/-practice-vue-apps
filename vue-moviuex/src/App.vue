4dcac8ca1a863972fbfa1aaa2a839f81ba58dd9d
IMM9O <islam.muhamed.90+github@gmail.com>
2018-01-15 05:36:51 +0200

    Add Movie-cell vue components

--- vue-moviuex/src/App.vue
+++ vue-moviuex/src/App.vue
@@ -1,6 +1,34 @@
 <template>
   <div id="app">
-    <h1>Vue moviuex app</h1>
-    {{ $store.state }}
+    <button @click="getPopular">Add Movie</button>
+    <div class="container-fluid">
+      <div class="row">
+          <Movie-cell class="col-xs-12 col-sm-6 col-md-3 col-lg-2 card" v-for="item in $store.state.movies" :key="item.id" :item="item" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+import MovieCell from './components/movie-cell'
+export default {
+  components: {
+    MovieCell
+  },
+  methods: mapActions([
+    'addMovie',
+    'getPopular'
+  ])
+}
+</script>
+
+<style>
+
+.card {
+  margin: 5px;
+}
+
+
+</style>
+
---
+++ vue-moviuex/src/components/movie-cell.vue
@@ -0,0 +1,58 @@
+<template>
+
+  <div class="movie-card">
+      <img :src="item.poster_path" class="poster cover" height="300" alt="Placeholder image">
+      <div class="caption">
+        <h3>{{ item.title }}</h3>
+        <p class="desc">{{ item.overview }}</p>
+      </div>
+   </div>
+
+</template>
+
+<script>
+export default {
+  name: 'MovieCell',
+  props: ['item']
+}
+</script>
+
+<style>
+.movie-card {
+  background: #000;
+  color: rgba(255, 255, 255, 0.85);
+  position: relative;
+  width: 600px;
+  border-radius: 5px;
+  box-shadow: 6px 4px 8px 0 rgba(0, 0, 0, 0.3), -6px 4px 8px 0 rgba(0, 0, 0, 0.3);
+  height: 250px;
+}
+
+.movie-card .poster {
+  position: absolute;
+  width: 200px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  left: 0;
+  filter: grayscale(80%);
+}
+
+.movie-card .caption {
+  left: 200px;
+  padding: 20px;
+  position: absolute;
+}
+
+.caption .title {
+
+}
+.caption .desc {
+
+}
+
+.cover {
+  height: 100%;
+  object-fit: cover;
+}
+</style>
+
