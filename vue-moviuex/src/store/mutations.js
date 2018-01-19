9f9f504e8b82927a9da05a2ab7c2ba0decdc21d9
IMM9O <islam.muhamed.90+github@gmail.com>
2018-01-19 10:08:59 +0200

    Check if the movie exist on the array before add it

--- vue-moviuex/src/store/mutations.js
+++ vue-moviuex/src/store/mutations.js
@@ -4,11 +4,11 @@ export const mutations = {
     [types.ADD_MOVIE] (state, payload){
         if(payload.id) {
             payload.poster_path = `https://image.tmdb.org/t/p/w500${payload.poster_path}`
-            state.movies.push(payload)    
+            if(checkIfExist(state.movies, payload.id)) state.movies.push(payload)    
         } else if(payload.results) {
             payload.results.forEach(element => {
                 element.poster_path = `https://image.tmdb.org/t/p/w500${element.poster_path}`;
-                state.movies.push(element);
+                 if(checkIfExist(state.movies, element.id)) state.movies.push(element);
             });
         }
 
@@ -17,4 +17,9 @@ export const mutations = {
         state.movies.splice(payload, 1)
     }
 
+}
+
+
+function checkIfExist(state, id){
+    return !state.filter(item => item.id === id).length;
 }
